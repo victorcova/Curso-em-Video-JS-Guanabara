6edc6749dc2da7429d71f2bc8976f597b9d65143
victorcova <victor.avoc@gmail.com>
2022-04-12 11:38:09 -0300

    Create ambiente.js

---
+++ MODULO-E/ambiente.js
@@ -0,0 +1,19 @@
+// console.log('start');
+
+var count = 1;
+
+//WHILE
+// while (count <= 20) {
+//     if (count == 1) {
+//         console.log(`Comi ${count} pedaço de pizza`);
+//     }else{
+//         console.log(`Comi ${count} pedaços de pizza`);
+//     }
+//     count++;
+// }
+
+// DO WHILE
+do {
+    console.log(`Número ${count}`);
+    count++;
+} while (count <= 20);
