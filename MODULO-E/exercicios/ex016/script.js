f53b3d1594d87df4b0317dcbf127bd19f27aaba2
victorcova <victor.avoc@gmail.com>
2022-04-12 17:14:33 -0300

    exercicio1completo

--- MODULO-E/exercicios/ex016/script.js
+++ MODULO-E/exercicios/ex016/script.js
@@ -8,22 +8,31 @@ function contar() {
     var fim = Number(document.getElementById('fim').value);
     var passo = Number(document.getElementById('passo').value);
     var res = document.querySelector('div#res');
-    
-    //o sistema vai contar do valor inicial até o final pulando no valor de passo:         
-    let a = ini;
-    let b = ini;    
-    res.innerHTML = `<strong>Contando<strong><br>`;
-    while(ini < fim){
-        if (a == ini) {            
-            res.innerHTML += `${a} <i class="fa-solid fa-person-walking"></i> `;
-            //console.log('valor de ini = '+ini+' / ini: ',a);
-            a = false;
-        }else if(b+passo <= fim){
-            res.innerHTML += `${b+passo} <i class="fa-solid fa-person-walking"></i> `;
-            //console.log('valor de ini = '+ini+' / result: ', (b+passo));
-            b = b+passo;            
+
+    if(ini == '' || fim == ''){
+        res.innerHTML = `<i class="fa-solid fa-circle-exclamation"></i> O <strong>início</strong> ou o <strong>fim</strong> não foram definidos`;     
+        res.style.color = 'red';
+    }else if(passo == 0){
+        res.innerHTML = `<i class="fa-solid fa-circle-exclamation"></i> A quantidade de <strong>passos</strong> não definida!`;
+        res.style.color = 'red';
+    }else{    
+        //o sistema vai contar do valor inicial até o final pulando no valor de passo:         
+        let a = ini;
+        let b = ini;    
+        res.innerHTML = `<strong>Contando:</strong><br>`;
+        while(ini <= fim){
+            if (a == ini) {            
+                res.innerHTML += `${a} <i class="fa-solid fa-person-walking"></i> `;
+                //console.log('valor de ini = '+ini+' / ini: ',a);
+                a = false;
+            }else if(b+passo <= fim){
+                res.innerHTML += `${b+passo} <i class="fa-solid fa-person-walking"></i> `;
+                //console.log('valor de ini = '+ini+' / result: ', (b+passo));
+                b = b+passo;            
+            }
+            ini++;        
         }
-        ini++;        
+        res.innerHTML += `<i class="fa-solid fa-flag-checkered"></i>`;
+        res.style.color = '#003641';
     }
-    
 }
